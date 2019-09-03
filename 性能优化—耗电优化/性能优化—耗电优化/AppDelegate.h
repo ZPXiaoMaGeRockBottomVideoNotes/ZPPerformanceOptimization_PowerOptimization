261374c9ffff11fa47b8d6991dfe3cead69fc3e3
赵鹏 <nemozp@163.com>
2019-09-03 13:56:04 +0800

    创建项目。

---
+++ 性能优化—耗电优化/性能优化—耗电优化/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  性能优化—耗电优化
+//
+//  Created by 赵鹏 on 2019/9/3.
+//  Copyright © 2019 赵鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
